9d0a50e2f41a5e2a0be7481834e03820beef2cae
Kseniya Grebenyuk <kgrebenyuk@caeonline.com>
2026-05-21 09:17:54 -0500

    Added first test for HomePage.

---
+++ Pages/BasePage.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Playwright;
+
+namespace PetClinicTests.Pages
+{
+    public abstract class BasePage
+    {
+        protected string END_POINT;
+
+        protected IPage _page;
+
+        public BasePage(IPage page) => _page = page;
+
+        protected abstract string GetEndpoint();
+
+        public async Task GotoAsync()
+        {
+            await _page.GotoAsync(Environment.GetEnvironmentVariable("URL") + GetEndpoint());
+        }
+    }
+}
---
+++ Pages/HomePage.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Playwright;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+
+namespace PetClinicTests.Pages
+{
+    public abstract class HomePage: BasePage
+    {
+        public HomePage(IPage page) : base(page) => END_POINT = "welcome";
+
+        private ILocator _welcomeToPetclinicText => _page.GetByRole('heading',{name:'Welcome to Petclinic'});
+        private ILocator _welcomeText => _page.GetByRole('heading',{name:'Welcome',exact:true});
+        private ILocator _welcomeImage => _page.GetByRole('img',{name:'pets logo'});
+
+        protected override string GetEndpoint()
+        {
+            return END_POINT;
+        }
+
+        public async Task GetWelcomePictureandText()
+        {
+            await _welcomeToPetclinicText;
+            await _passwordInput.FillAsync(user.Password);
+            await _logInButton.ClickAsync();
+            await _userloginButton(user).IsEnabledAsync();
+        }
+
+        public async Task WaitForHomePage()
+        {
+            await _userNameInput.WaitForAsync();
+        }
+    }
+}
